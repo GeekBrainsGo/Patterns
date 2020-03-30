7b241b643fe2a8e55a859050cd8a103811e77696
Dmitrii Fadeev <dmitrii.fadeev@sebn.com>
2020-03-30 21:19:58 +0300

    lesson 3.1 done

--- event_channel/publisher.go
+++ event_channel/publisher.go
@@ -16,14 +16,26 @@ func NewPublisher() *Publisher {
 	}
 }
 
-func(p *Publisher) AddChannel(name string, channel *Channel) {
+func (p *Publisher) AddChannel(name string, channel *Channel) {
 	p.channels[name] = channel
 }
 
 //TODO: Удаление канала и получение списка каналов
+func (p *Publisher) RemoveChannel(channel string) error {
+	if _, ok := p.channels[channel]; ok {
+		delete(p.channels, channel)
+		return nil
+	}
+	return fmt.Errorf("channel %s not found", channel)
+}
 
 //TODO: Если 0 имён каналов - отправлять всем
-func(p *Publisher) Send(msg string, channels ...string) error {
+func (p *Publisher) Send(msg string, channels ...string) error {
+	if len(channels) == 0 {
+		for _, channel := range p.channels {
+			channel.Send(msg)
+		}
+	}
 	for _, ch := range channels {
 		channel, ok := p.channels[ch]
 		if !ok {

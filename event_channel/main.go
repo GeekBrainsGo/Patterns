3b3984123b5bdd93716a86f95a6143e4d37bc37d
Alexeybobkov47 <Alexeybobkov47@gmail.com>
2020-03-31 14:56:07 +0400

    done

--- event_channel/main.go
+++ event_channel/main.go
@@ -13,16 +13,26 @@ func main() {
 
 	ch2 := NewChannel()
 	ch2.Subscribe(user2)
-
+	ch3 := NewChannel()
 	pub := NewPublisher()
 	pub.AddChannel("test", ch1)
 	pub.AddChannel("test2", ch2)
+	pub.AddChannel("testDelete", ch3)
+	pub.ShowChannels()
+	pub.DeleteChannel("testDelete")
+	pub.ShowChannels()
 
 	if err := pub.Send("HELLO!", "test"); err != nil {
 		log.Fatalf("can't send: %s", err)
 	}
 
 	if err := pub.Send("HELLO FROM CH2", "test2"); err != nil {
+		log.Fatalf("can't send: %s", err)
+	}
+	if err := pub.Send("Привет во все каналы!", ""); err != nil {
+		log.Fatalf("can't send: %s", err)
+	}
+	if err := pub.SendAll("Привет во все каналы 2!"); err != nil {
 		log.Fatalf("can't send: %s", err)
 	}
 
